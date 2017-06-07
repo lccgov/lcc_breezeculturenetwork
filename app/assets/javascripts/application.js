75d3c380d6a5d68e3b7e64d7ae1f3ba9690818a8
Travis CI <developer@leeds.gov.uk>
2017-06-07 13:58:44 +0100

    added google analytics js to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,2 @@
-//= require jquery.validate
-//= require handlebars
-//= require bcn-settings
-//= require bcn-googlemaps
-//= require bcn-searchresults
-//= require bcn-datepicker
-//= require date-range-filter
-//= require keyword-filter
-//= require news-archive-filter
-
-
+//Application specific javascript to go here
+//= require google-analytics
