8f13f9705f4ede9037c53487f4913760e75b8bdf
David Hunter <david.hunter@leeds.gov.uk>
2017-09-25 13:13:52 +0100

    updated packages and application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,11 @@
-//Application specific javascript to go here
+//= require jquery.validate
+//= require handlebars
+//= require date-range-filter
+//= require keyword-filter
+//= require news-archive-filter
+//= require page-comments
+//= require contact-us
+//= require survey
 //= require google-analytics
+//= require google-maps
+//= require resize-search-results
